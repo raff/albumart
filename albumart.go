ed271db9b1eaf1668a4d25ee50ae4922bbcd4258
Raffaele Sena <raff367@gmail.com>
2023-02-26 20:00:12 -0800

    Added GetArtworkFlags, that allow to specify what type of entity (song or album) and size to fetch.

--- albumart.go
+++ albumart.go
@@ -1,35 +1,52 @@
 package albumart
 
 import (
-	"net/http"
 	"fmt"
+	"github.com/buger/jsonparser"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"regexp"
-	"github.com/buger/jsonparser"
 )
 
 func GetArtwork(ArtistName string, AlbumSongName string) string {
+	query := AlbumSongName
+	if ArtistName != "" {
+		query = ArtistName + " " + query
+	}
+
+	uri := GetArtworkFlags(query, "", true)
+        if uri == "" {
+            return "Not Found"
+        }
+
+        return uri
+}
+
+func GetArtworkFlags(query, entity string, large bool) string {
+	if entity == "" {
+		entity = "song"
+	}
+
 	artworkUrlToReturn := ""
 	whitespaceRegEx, _ := regexp.Compile(`\s`)
-	ArtistName = whitespaceRegEx.ReplaceAllString(ArtistName, "+")
-	AlbumSongName = whitespaceRegEx.ReplaceAllString(AlbumSongName, "+")
-	response, err := http.Get("https://itunes.apple.com/search?term=" + ArtistName + "+" + AlbumSongName + "&limit=1&entity=song")
-  if err != nil {
-    fmt.Printf("%s", err)
-  } else {
+	query = whitespaceRegEx.ReplaceAllString(query, "+")
+	response, err := http.Get("https://itunes.apple.com/search?term=" + query + "&limit=1&entity=" + entity)
+	if err != nil {
+		fmt.Printf("%s", err)
+	} else {
 		defer response.Body.Close()
 		data, readErr := ioutil.ReadAll(response.Body)
 		if readErr != nil {
 			log.Fatal(readErr)
 		}
 		resultCount, _ := jsonparser.GetInt(data, "resultCount")
-		if resultCount == 0 {
-			artworkUrlToReturn = "Not Found"
-		} else {
+		if resultCount > 0 {
 			artworkUrlToReturn, _ = jsonparser.GetString(data, "results", "[0]", "artworkUrl100")
-			makeLarge, _ := regexp.Compile("100x100bb")
-  		artworkUrlToReturn = makeLarge.ReplaceAllString(artworkUrlToReturn, "1200x1200bb")
+			if large {
+				makeLarge, _ := regexp.Compile("100x100bb")
+				artworkUrlToReturn = makeLarge.ReplaceAllString(artworkUrlToReturn, "1200x1200bb")
+			}
 		}
 	}
 	return artworkUrlToReturn
